4c13e89113cae89d47abfa30c8795a493be1db80
Bryan McAfee <brymcafee@gmail.com>
2017-11-09 14:12:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,18 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+win_combinations =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [8,4,0],
+  [6,4,2],
+]
+
+def won?(board)
+  win_combinations.find do |win|
+
+  end
